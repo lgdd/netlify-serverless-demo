efb8ebaea3db263bcd2befea59b3fe0eb1976596
lgdd <louis-guillaume.durand@liferay.com>
2021-12-17 18:25:20 +0100

    chore: initial commit

---
+++ express/server.js
@@ -0,0 +1,24 @@
+'use strict';
+const express = require('express');
+const serverless = require('serverless-http');
+const app = express();
+const bodyParser = require('body-parser');
+const router = express.Router();
+
+app.use(bodyParser.json());
+app.use('/.netlify/functions/server', router); // path must route to lambda
+app.use('/', router);
+
+router.get('/', (req, res) => {
+  res.writeHead(200, { 'Content-Type': 'application/json' });
+  res.write(`{
+    "delaiMoyenSejourGlobal": "12J",
+    "nbrConteneurs": "120000",
+    "nbrConteneursInspectes": "349",
+    "nbrConteneursInspectes": "349"
+  }`);
+  res.end();
+});
+
+module.exports = app;
+module.exports.handler = serverless(app);
